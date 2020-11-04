ea111be7808b0b1b99e86e38d99b57065e51595b
Constance <hytan.2018@sis.smu.edu.sg>
2020-11-05 01:37:59 +0800

    UI mockup (add logo, sidebar, navigation pane, table)

--- shinyapp/prototype/ui.R
+++ shinyapp/prototype/ui.R
@@ -1,12 +1,39 @@
-navbarPage("title",
+PAGE_TITLE <- "Find schools near you"
 
-    tabPanel("Plot"),
+fluidPage(theme="simplex",
+    # shinythemes::themeSelector(),
+
+    titlePanel(
+        windowTitle = PAGE_TITLE,
+        title = div(
+            img(src = "logo.png", height = 100, width = 100, style = "margin: 10px 10px"),
+            PAGE_TITLE
+        )
+    ),
 
     sidebarLayout(
-        sidebarPanel("Display"),
+        sidebarPanel("",
+             selectizeInput("region", "Region:", c("North", "South", "East", "West"),
+                            multiple = TRUE, options = list(
+                                'plugins' = list('remove_button'),
+                                'create' = TRUE,
+                                'persist' = FALSE)
+             ),
+             selectizeInput("jcs", "Junior College(s):", c("W", "I", "P"),
+                            multiple = TRUE, options = list(
+                                'plugins' = list('remove_button'),
+                                'create' = TRUE,
+                                'persist' = FALSE)
+             ),
+            selectInput("analysis", "Analysis:", c("", "Hot & Cold Spot Areas (Gi*)", "Distance (Hansen Accessibility)", "Spatial Accessibility Measure (SAM)")),
+            checkboxGroupInput("display", "Display:", c("Show school points", "Show HDB points"))
+        ),
         mainPanel(
-            plotOutput("mapPlot"),
-            tmapOutput("tmapPlot")
+            navbarPage("Information", collapsible=TRUE,
+                tabPanel("Interactive Map", tmapOutput("tmapPlot"), width = "100%", height = "100%"),
+                tabPanel("Static Map", plotOutput("mapPlot"), width = "100%", height = "100%"),
+                tabPanel("Schools Details", tableOutput("schoolsTable"))
+            )
         )
     )
 
