93bb7c5b01fd2748db8943570561669db1ebb757
jasxmine <54893485+jasxmine@users.noreply.github.com>
2020-11-28 18:54:31 +0800

    changed address to camelcase

--- shinyapp/app/global.R
+++ shinyapp/app/global.R
@@ -74,6 +74,7 @@ colnames(hdb_df)[7] <- "Y"
 hdb_df$X <- coordinates_val[,2]
 hdb_df$Y <- coordinates_val[,1]
 
+hdb_df$ADDRESS <- rapportools::tocamel(tolower(hdb_df$ADDRESS), upper=TRUE, sep=" ")
 hdb<-hdb_df
 # UI
 schIcon <- makeIcon(
